aefdc710c40f530115b4bfd6a5fa4478fb08f92b
samadhan7 <samadhanjejurkar78@gmail.com>
2024-09-29 19:42:42 +0530

    migrations performed

---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ServerLibrary.Data
+{
+	public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+	{
+
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Branch> Branches { get; set; }
+		public DbSet<Town> Towns { get; set; }
+		public DbSet<ApplicationUser>  ApplicationUsers { get; set; }
+	}
+}
